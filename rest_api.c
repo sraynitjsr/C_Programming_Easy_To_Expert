57d24fb7bd3e8b4128eff807f26a6a8f3e3112c6
unknown <subhradeepray2017@gmail.com>
2023-06-21 01:12:01 +0530

    REST API Using C

---
+++ rest_api.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <microhttpd.h>
+
+int request_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls) {
+    const char *response = "Hello, World!"; // Replace with your desired response
+    struct MHD_Response *http_response = MHD_create_response_from_buffer(strlen(response), (void *)response, MHD_RESPMEM_PERSISTENT);
+    int ret = MHD_queue_response(connection, MHD_HTTP_OK, http_response);
+    MHD_destroy_response(http_response);
+    return ret;
+}
+
+int main() {
+    struct MHD_Daemon *daemon = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION, 8080, NULL, NULL, &request_handler, NULL, MHD_OPTION_END);
+    if (daemon == NULL) {
+        fprintf(stderr, "Error starting the server.\n");
+        return 1;
+    }
+    getchar(); // Wait for user input (e.g., press Enter) to stop the server
+    MHD_stop_daemon(daemon);
+    return 0;
+}
