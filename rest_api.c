454c425734432266c92685775e958f4c5eacc457
unknown <subhradeepray2017@gmail.com>
2023-06-29 11:04:43 +0530

    REST API Using C 'microhttpd' Library

--- rest_api.c
+++ rest_api.c
@@ -4,7 +4,7 @@
 #include <microhttpd.h>
 
 int request_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls) {
-    const char *response = "Hello, World!"; // Replace with your desired response
+    const char *response = "Custom Response or Any Response";
     struct MHD_Response *http_response = MHD_create_response_from_buffer(strlen(response), (void *)response, MHD_RESPMEM_PERSISTENT);
     int ret = MHD_queue_response(connection, MHD_HTTP_OK, http_response);
     MHD_destroy_response(http_response);
@@ -17,7 +17,7 @@ int main() {
         fprintf(stderr, "Error starting the server.\n");
         return 1;
     }
-    getchar(); // Wait for user input (e.g., press Enter) to stop the server
+    getchar();
     MHD_stop_daemon(daemon);
     return 0;
 }
