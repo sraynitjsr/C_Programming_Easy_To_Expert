60f506abe8b5b8e8919c7c1de8a7c2f90caf78ff
unknown <subhradeepray2017@gmail.com>
2023-06-29 11:29:00 +0530

    Callback Mechanism in C

--- README.md
+++
@@ -1,3 +0,0 @@
-## Learning C Programming From Zero to Hero
-
-## Function Pointer and Pointer to a Function in C are Different, Check .c Files With Code
--- callback.c
+++ callback.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 // Definition of the callback function type
-typedef void (*Callback)(int);
+typedef void (*Callback)(int, int);
 
 // Function that takes a callback function as an argument
 void performOperation(int value, Callback callback) {
-    printf("Performing operation with value: %d\n", value);
+    printf("Performing area calculation with radius => %d\n", value);
     
     // Call the callback function
-    callback(value*2);
+    callback(value, 3.14*value*value);
 }
 
 // Callback function to be passed
-void callbackFunction(int value) {
-    printf("Callback function called with value: %d\n", value);
+void callbackFunctionAreaOfCircle(int radius, int area) {
+    printf("Area of the circle with radius %d is => %d \n", radius, area);
 }
 
 int main() {
     int data = 10;
 
     // Call performOperation and pass the callback function
-    performOperation(data, callbackFunction);
+    performOperation(data, callbackFunctionAreaOfCircle);
 
     return 0;
 }
