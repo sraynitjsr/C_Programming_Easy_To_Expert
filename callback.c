8a1047c994db703c00c485b7d1019183e04aacd0
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-12-22 23:44:13 +0530

    Talking to REST APIs Using Token in C Programming

--- callback.c
+++ callback.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
+#include <curl/curl.h>
 
-// Definition of the callback function type
-typedef void (*Callback)(int, int);
-
-// Function that takes a callback function as an argument
-void performOperation(int value, Callback callback) {
-    printf("Performing area calculation with radius => %d\n", value);
-    
-    // Call the callback function
-    callback(value, 3.14*value*value);
-}
-
-// Callback function to be passed
-void callbackFunctionAreaOfCircle(int radius, int area) {
-    printf("Area of the circle with radius %d is => %d \n", radius, area);
-}
+#define API_ENDPOINT "https://jsonplaceholder.typicode.com/todos/1"
+#define API_TOKEN "my_api_token_here"
 
 int main() {
-    int data = 10;
+    CURL *curl;
+    CURLcode res;
 
-    // Call performOperation and pass the callback function
-    performOperation(data, callbackFunctionAreaOfCircle);
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    curl = curl_easy_init();
+    if (!curl) {
+        fprintf(stderr, "Error initializing curl.\n");
+        return 1;
+    }
+
+    curl_easy_setopt(curl, CURLOPT_URL, API_ENDPOINT);
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, stdout);
+
+    struct curl_slist *headers = NULL;
+    char auth_header[50];
+    snprintf(auth_header, sizeof(auth_header), "Authorization: Bearer %s", API_TOKEN);
+    headers = curl_slist_append(headers, auth_header);
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+    res = curl_easy_perform(curl);
+
+    if (res != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+    curl_easy_cleanup(curl);
+    curl_slist_free_all(headers);
+    curl_global_cleanup();
 
     return 0;
 }
