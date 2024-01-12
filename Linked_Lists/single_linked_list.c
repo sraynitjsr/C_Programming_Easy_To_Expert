609a808cc0d1fe6dcf9f97e9e37e7d4ccc7670ed
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-01-12 14:32:20 +0530

    Single Linked List => insertAtBeginning, deleteAtBeginning and display methods

---
+++ Linked_Lists/single_linked_list.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtBeginning(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    newNode->next = *head;
+    *head = newNode;
+    printf("Inserted %d at the beginning\n", value);
+}
+
+void deleteAtBeginning(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty. Cannot delete.\n");
+        return;
+    }
+    struct Node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+    printf("Deleted the first node\n");
+}
+
+void display(struct Node* head) {
+    printf("Linked List: ");
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertAtBeginning(&head, 5);
+    insertAtBeginning(&head, 10);
+    insertAtBeginning(&head, 15);
+    display(head);
+
+    deleteAtBeginning(&head);
+    display(head);
+
+    return 0;
+}
